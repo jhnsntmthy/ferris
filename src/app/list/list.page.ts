10a00ef6e694a423c3a2aa44017beaab53af7083
Cave Johnson <jhnsntmthy@gmail.com>
2019-03-08 15:38:32 -0500

    for loops suck

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -21,13 +21,11 @@ export class ListPage implements OnInit {
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.items = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map( (c, i) => ({
+      title: 'Item ' + c,
+      note: 'This is item #' + i,
+      icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    }));
   }
 
   ngOnInit() {
