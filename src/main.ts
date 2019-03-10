5b3a3e273bc227474f82005a6e4cf7a46a5e762b
Cave Johnson <jhnsntmthy@gmail.com>
2019-03-10 19:20:24 -0400

    some updates to make WC work

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare global {
+  interface Window {
+    WebComponents: {
+      ready: boolean;
+    };
+  }
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const bootstrapIonicApp = () => {
+  console.log('bootstrapIonicApp()');
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+}
+
+if (window.WebComponents!.ready) {
+  bootstrapIonicApp();
+} else {
+  window.addEventListener('WebComponentsReady', bootstrapIonicApp);
+}
