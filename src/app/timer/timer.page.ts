19aea9fc9c63cc7f62653d0613881a8ebe446fbd
Cave Johnson <jhnsntmthy@gmail.com>
2019-03-10 20:31:25 -0400

    updated the timer to work, pretty well sadly the slider is not working

--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import '@authentic/mwc-circular-progress';
 import '@authentic/mwc-button';
+import '@authentic/mwc-chips';
+import '@authentic/mwc-slider';
 
 @Component({
   selector: 'app-timer',
@@ -22,17 +24,55 @@ export class TimerPage implements OnInit {
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   public dial_progress: number = 0;
+  public lapElapsed: number = 0;
+  public totalElapsed: number = 0;
+  public totalStr: string = '0s';
   private totalTimer: number;
   private lapTimer: number;
+  private smoothness: number = 100;
+  private speedFactor: number = 1; // increase this to speed up the clock
+  private lapTime: number = 90000; // 1.5 minutes
 
   constructor() {
-    this.dial_progress = 45;
+    this.dial_progress = 100;
+  }
+
+  startTimer() {
+    this.resetAll();
+    this.totalTimer = setInterval(this.tick.bind(this), this.smoothness);
+  }
+
+  resetAll() {
+    clearInterval(this.totalTimer);
+    this.totalElapsed = 0;
+    this.lapElapsed = 0;
+  }
+
+  elapse() {
+    return this.smoothness * this.speedFactor;
+  }
+
+  tick() {
+    this.totalElapsed += this.elapse();
+    this.checkForLap();
+    this.translateTotal(this.totalElapsed);
+  }
+
+  checkForLap() {
+    this.lapElapsed += this.elapse();
+    this.lapElapsed = this.lapElapsed > this.lapTime ? 0 : this.lapElapsed
+    this.dial_progress = 100 * this.lapElapsed / this.lapTime;
+  }
+
+  translateTotal(s: number) {
+    const ms = this.totalElapsed % 1000;
+    s = (s - ms) / 1000;
+    const secs = s % 60;
+    s = (s - secs) / 60;
+    const mm = s;
+    this.totalStr = mm + 'm ' + secs + 's';
   }
 
   ngOnInit() {
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
 }
